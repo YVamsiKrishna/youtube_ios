ac7b16a9b5f792bfc73dc6010d136844bf2c9053
Vamsi Krishna <Vamsi@zeta-mac-mini.local>
2015-11-20 14:36:39 +0530

    Initial Commit

---
+++ YouTube/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  YouTube
+//
+//  Created by Vamsi Krishna on 20/11/15.
+//  Copyright © 2015 Vamsi Krishna. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ YouTubeTests/YouTubeTests.m
@@ -0,0 +1,39 @@
+//
+//  YouTubeTests.m
+//  YouTubeTests
+//
+//  Created by Vamsi Krishna on 20/11/15.
+//  Copyright © 2015 Vamsi Krishna. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface YouTubeTests : XCTestCase
+
+@end
+
+@implementation YouTubeTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
